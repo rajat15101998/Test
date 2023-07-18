ce4635574a42b15aea18a9e87713a6df084a4609
Rajat Singh <rajatrs1510@gmail.com>
2023-07-18 17:48:11 +0530

    updated Employee File

--- Employee.java
+++ Employee.java
@@ -1,16 +1,14 @@
-import java.util.*;
-
 public class Employee {
 	private int id;
 	private String name;
 	
 	Employee() {
-		System.out.println("Employee Created");
 	}
 	
 	Employee(int id, String name) {
 		this.id = id;
 		this.name = name;
+		System.out.println("Employee Created with id:" + this.id + " and name:" + this.name);
 	}
 	
 	public static void main(String[] args) {
