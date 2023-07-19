33c6a72edd8b94d53dac55bcdf598069229b6f16
Rajat Singh <rajatrs1510@gmail.com>
2023-07-19 19:16:21 +0530

    increased n from 20 to 100

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 public class Student {
 	public static void main(String[] args) {
-		int n = 20;
+		int n = 100;
 		for(int i = 1; i <= n; i++) {
 			if(i%2 == 0)
 			   System.out.println("Even Roll Number");
