3972268dee0ea61e70c85597505b9df027de1d78
Rajat Singh <rajatrs1510@gmail.com>
2023-07-19 19:04:20 +0530

    updated n based on Odd Even

--- Student.java
+++ Student.java
@@ -1,9 +1,11 @@
-
 public class Student {
 	public static void main(String[] args) {
-		int n = 15;
-		for(int i = 0; i < n; i++) {
-			System.out.println(i);
+		int n = 20;
+		for(int i = 1; i <= n; i++) {
+			if(i%2 == 0)
+			   System.out.println("Even Roll Number");
+			else
+			   System.out.println("Odd Roll Number");  
 		}
 	}
 }
